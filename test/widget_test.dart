399a57bd2457a0248aadd68634baebd793eae940
Vajinepalli Sai  Manivardhan <118188619+manivardhan7@users.noreply.github.com>
2025-11-12 12:27:27 +0530

    Update widget_test.dart - Fix import path to nexacore_mobile_platform

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_development_project/main.dart';
+import 'package:nexacore_mobile_platform/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
